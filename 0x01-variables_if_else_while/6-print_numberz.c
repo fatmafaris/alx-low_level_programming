f4b48f47d8766708d2f82ad67b3fbb27f50c8756
fatmafaris <smsma4644@gmail.com>
2023-12-15 17:58:38 -0800

    Write a program that prints all single digit numbers of base 10 starting from 0, followed by a new line, You can only use the putchar function twice, You are not allowed to use any variable of type char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: prints all single digit numbers of base 10 starting from 0
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
