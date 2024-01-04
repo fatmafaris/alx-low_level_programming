b0b5537dcd43d84e989ee8e7a84412112524c773
fatmafaris <smsma4644@gmail.com>
2024-01-04 03:54:43 -0800

    Write a program that prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
  * main - Entry point
  *
- * Description: prints _putchar using putchar prototype
+ * Description: prints _putchar
  *
  * Return: Always 0 (Success)
 */
