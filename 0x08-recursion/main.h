50d4d05433429e058ec43174d97a5a26a72c2b2e
fatmafaris <smsma4644@gmail.com>
2024-01-13 09:23:29 -0800

    Write a function that returns 1 if the input integer is a prime number, otherwise return 0

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -10,6 +10,7 @@ int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int sqrt_helper(int n, int start, int end);
 int is_prime_number(int n);
+int prime2(int a, int b);
 int is_palindrome(char *s);
 int wildcmp(char *s1, char *s2);
 
