bf32625b83fa1d2109b00eb478ac26193aaa7751
fatmafaris <smsma4644@gmail.com>
2024-01-19 13:41:49 -0800

    Write a program that multiplies two positive numbers,Usage: mul num1 num2,num1 and num2 will be passed in base 10,Print the result, followed by a new line,If the number of arguments is incorrect, print Error, followed by a new line, and exit with a status of 98,num1 and num2 should only be composed of digits. If not, print Error, followed by a new line, and exit,with a status of 98,You are allowed to use more than 5 functions in your file

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,86 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ *
+ * @str: string
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+}
+/**
+ * _atoi - convert string to integer
+ *
+ * @s: char type string
+ *
+ * Return: integer converted
+ */
+int _atoi(const char *s)
+{
+	int sign = 1;
+	unsigned long int response = 0, firstnum, i;
+
+	for (firstnum = 0; !(s[firstnum] >= 48 && s[firstnum] <= 57); firstnum++)
+	{
+		if (s[firstnum] == '-')
+		{
+			sign *= -1;
+		}
+	}
+
+	for (i = firstnum; s[i] >= 48 && s[i] <= 57; i++)
+	{
+		response *= 10;
+		response += (s[i] - 48);
+	}
+	return (sign * response);
+}
+/**
+ * print_int - prints integer
+ *
+ * @n: int
+ *
+ * Return: 0
+ */
+void print_int(unsigned long int n)
+{
+	unsigned long int divisor = 1, i, response;
+
+	for (i = 0; n / divisor > 9; i++, divisor *= 10)
+	;
+
+	for (; divisor >= 1; n %= divisor, divisor /= 10)
+	{
+		response = n / divisor;
+		_putchar('0' + response);
+	}
+}
+/**
+ * main - print the result of multiplication
+ *
+ * @argc: integer
+ * @argv: list
+ *
+ * Return: 0
+ */
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+
+	if (argc != 3)
+	{
+		_puts("Error ");
+		exit(98);
+	}
+	print_int(_atoi(argv[1]) * _atoi(argv[2]));
+	_putchar('\n');
+
+	return (0);
+}
