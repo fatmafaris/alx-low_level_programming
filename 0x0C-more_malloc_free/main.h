305a26da1b145ead66029a1d345fbd1a244eaa3e
fatmafaris <smsma4644@gmail.com>
2024-01-19 14:17:14 -0800

    Write a program that multiplies two positive numbers,Usage: mul num1 num2,num1 and num2 will be passed in base 10,Print the result, followed by a new line,If the number of arguments is incorrect, print Error, followed by a new line, and exit with a status of 98,num1 and num2 should only be composed of digits. If not, print Error, followed by a new line, and exit,with a status of 98,You are allowed to use more than 5 functions in your file

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,14 +3,13 @@
 #include <stdlib.h>
 
 int _putchar(char c);
+void _puts(char *str);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int main(int argc, char const *argv[]);
-void print_int(int n);
 int _atoi(const char *s);
-void _puts(char *str);
+void print_int(int n);
 
 #endif
